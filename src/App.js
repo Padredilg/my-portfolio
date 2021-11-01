d64af1cf8c42ad20b55f3b653e5e3eb0372fa198
Padredilg <padredilg@knights.ucf.edu>
2021-10-31 21:47:27 -0400

    attempting to create About

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import backgroundVideo from './components/BackgroundVideo/FallingStars.mp4';
+import Nav from './components/Nav';
+import About from './components/About';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
           <source src={backgroundVideo} type="video/mp4" />
       </video>
 
+      <About />
+      
     </div>
   );
 }
