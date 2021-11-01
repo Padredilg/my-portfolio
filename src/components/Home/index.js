3ff44e7382de8dba7b726c17a6ed63926538ba43
Padredilg <padredilg@knights.ucf.edu>
2021-11-01 13:13:01 -0400

    improved Home css

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ const Home = () =>{
             <img className="profile-pic" src={profilePic} alt="Luiz" />
             <div className="greeting">
                 <h1><span>Hi,</span> I'm Luiz!</h1>
-                <h2>I develop softwares and create websites!</h2>
+                <h2>I develop softwares and create websites :)</h2>
             </div>
             <a className="know-more" href="#about">Would you like to know more about me?</a>
         </div>
