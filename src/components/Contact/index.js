a7fe9164b54c22993833384b66f09a71701ef4e6
Padredilg <padredilg@knights.ucf.edu>
2021-11-03 09:03:20 -0400

    fixed css on Home

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -39,7 +39,7 @@ function ContactForm() {
 
     return (
         <section>
-            <h1 data-testid="h1tag" >Contact me</h1>
+            <h1 id="Contact" >Contact me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 {/* Name */}
                 <div>
