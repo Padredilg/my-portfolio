3cabb8d937db717ee9a22597391bf8eedb47476e
Padredilg <padredilg@knights.ucf.edu>
2021-11-06 10:16:56 -0400

    added hover effect on projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,17 +7,19 @@ function Projects({ category }) {
     {
       index: 0,
       name: 'Campfire',
+      shortdesc:'Social Media for Coding Bootcampers',
       tech: 'HTML, CSS, JavaScript, Bootstrap Framework, Node.js, Express.js, MySQL, Sequelize ORM, Bcrypt, express-session, Handlebars, Socket.IO, Browserify, Uppy.',
       description: 'Social media for Bootcamp students and tech lovers. Users can create an account, check every post ever created, create their own posts and interact with others, personalize their profile page, and interact with other users currently online through a global chat.',
       github: 'https://github.com/Padredilg/campfire',
       deployed: 'https://campfire-social-media.herokuapp.com/'
-      
+
     },
     {
       index: 1,
       name: 'Decide Eat',
+      shortdesc:'Date night-in app',
       tech: 'HTML, CSS, JavaScript, jQuery, Bulma, Spoonacular API, Spotify API',
-      description: 'Date night-in app. Find the perfect combination of food and music for your dinner night. Users can search recipes by their names or cuisine styles, as well as take a quiz to help them decide what to do for dinner, cocktail and also what Spotify playlist to listen to.',
+      description: 'Find the perfect combination of food and music for your dinner night. Users can search recipes by their names or cuisine styles, as well as take a quiz to help them decide what to do for dinner, cocktail and also what Spotify playlist to listen to.',
       github: 'https://github.com/Padredilg/decide-eat',
       deployed: 'https://padredilg.github.io/decide-eat/'
     },
@@ -36,20 +38,31 @@ function Projects({ category }) {
     <div>
       <div className="projects-wrapper">
         {projects.map((project, i) => (
-          <div>
-            <Button onClick={()=>{ handleShow(projects[i]) }}>
+          <div className="button-wrapper">
+            <Button
+              className="feat-project"
+              variant="link"
+              onClick={() => { handleShow(projects[i]) }}
+            >
               <img
                 src={require(`../../assets/projects/${i}.png`).default}
                 alt={project.name}
                 className="primary-projects"
                 key={project.name}
               />
+
+              <div class="portfolio-info"> 
+                <p className="read-more-hover">Read More</p>
+                <h6 className="project-title-hover">{project.name}</h6>
+                <p className="shortdesc">{project.shortdesc}</p>
+              </div>
+
             </Button>
           </div>
         ))}
       </div>
 
-    {show? <Modal currentProject={currentProject} onClose={ handleClose } />:null}
+      {show ? <Modal currentProject={currentProject} onClose={handleClose} /> : null}
     </div>
   )
 }
