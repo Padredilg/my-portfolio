0493bdb8b409c7ddc82d1003cebf5dd53df0d1a3
Padredilg <padredilg@knights.ucf.edu>
2021-11-07 09:56:03 -0500

    finished styles on contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,106 +4,104 @@ import { Container, Row, Col } from "react-bootstrap";
 
 
 function ContactForm() {
-    const [formState, setFormState] = useState({ name: '', email: '', subject: '', message: '' });
-    const { name, email, subject, message } = formState;
+  const [formState, setFormState] = useState({ name: '', email: '', subject: '', message: '' });
+  const { name, email, subject, message } = formState;
 
-    const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-    function handleChange(e) {
-        if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-            console.log(isValid);
-            // isValid conditional statement
-            if (!isValid) {
-                setErrorMessage('Your email is invalid.');
-            }
-            else {
-                setErrorMessage('');
-            }
-        }
-        else {
-            if (!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`);
-            } else {
-                setErrorMessage('');
-            }
-        }
-
-        if (!errorMessage) {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      }
+      else {
+        setErrorMessage('');
+      }
+    }
+    else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
     }
+  }
 
-    return (
-        <section className="section last-section" id="Contact">
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
 
-            <div className="section-heading">
-                <h2>Conn<em>ect</em></h2>
-                <p>Like what you see? Let's work together or just say hi! Either way I'd love to hear from you!</p>
-            </div>
+  return (
+    <section className="section last-section" id="Contact">
 
-            <Container>
-                <Row>
-                    <Col sm={7} md={5} className="m-auto">
-                        <img
-                            src={require(`../../assets/images/profile-pic-helmet.png`).default}
-                            alt='me with an astronaut helmet'
-                            className="image-of-me"
-                        />
-                    </Col>
-                    <Col xs={7} className="m-auto">
-                        <form id="contact-form" onSubmit={handleSubmit}>
+      <div className="section-heading">
+        <h2>Conn<em>ect</em></h2>
+        <p>Like what you see? Let's work together or just say hi! Either way I'd love to hear from you!</p>
+      </div>
 
-                            <div className="name-email-wrapper">
-                                {/* Name */}
-                                <div className="contact-tag-wrapper">
-                                    <label htmlFor="name"></label>
-                                    <input className="contact-tag" placeholder="Your name" type="text" defaultValue={name} onBlur={handleChange} name="name" />
-                                </div>
+      <Container>
+        <Row>
+          <Col md={5} className="m-auto">
+            <img
+              src={require(`../../assets/images/profile-pic-helmet.png`).default}
+              alt='me with an astronaut helmet'
+              className="image-of-me"
+            />
+          </Col>
+          <Col sm={12} md={7} className="m-auto">
+            <form id="contact-form" onSubmit={handleSubmit}>
 
-                                {/* Email */}
-                                <div className="contact-tag-wrapper">
-                                    <label htmlFor="email"></label>
-                                    <input className="contact-tag" placeholder="Your email" type="email" defaultValue={email} onBlur={handleChange} name="email" />
-                                </div>
-                            </div>
+                {/* Name */}
+              <div className="contact-tag-wrapper">
+                <label htmlFor="name"></label>
+                <input className="contact-tag" placeholder="Your name" type="text" defaultValue={name} onBlur={handleChange} name="name" />
+              </div>
 
-                            {/* Subject */}
-                            <div className="contact-tag-wrapper">
-                                <label  htmlFor="subject"></label>
-                                <input className="contact-tag" placeholder="Subject" name="subject" defaultValue={subject} onBlur={handleChange} rows="5" />
-                            </div>
-                            
+              {/* Email */}
+              <div className="contact-tag-wrapper">
+                <label htmlFor="email"></label>
+                <input className="contact-tag" placeholder="Your email" type="email" defaultValue={email} onBlur={handleChange} name="email" />
+              </div>
 
-                            {/* Message */}
-                            <div className="contact-tag-wrapper">
-                                <label  htmlFor="message"></label>
-                                <textarea className="contact-tag" placeholder="Message" name="message" defaultValue={message} onBlur={handleChange} rows="5" />
-                            </div>
+              {/* Subject */}
+              <div className="contact-tag-wrapper">
+                <label htmlFor="subject"></label>
+                <input className="contact-tag" placeholder="Subject" name="subject" defaultValue={subject} onBlur={handleChange} rows="5" />
+              </div>
 
 
-                            {/* error message */}
-                            {errorMessage && (
-                                <div>
-                                    <p className="error-text">{errorMessage}</p>
-                                </div>
-                            )}
-                            {/* submit */}
-
-                            <button type="submit">Submit</button>
-                        </form>
-                    </Col>
-                </Row>
-            </Container>
+              {/* Message */}
+              <div className="contact-tag-wrapper">
+                <label htmlFor="message"></label>
+                <textarea className="contact-tag" placeholder="Message" name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+              </div>
 
 
-        </section>
-    )
+              {/* error message */}
+              {errorMessage && (
+                <div>
+                  <p className="error-text">{errorMessage}</p>
+                </div>
+              )}
+              {/* submit */}
+
+              <button className="contact-submit" type="submit">Submit</button>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+
+
+    </section>
+  )
 }
 
 export default ContactForm;
